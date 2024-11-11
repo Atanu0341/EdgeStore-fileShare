c95a480a75545a5fe4776bc0dda5a58f68ebf8f5
Atanu Majumder <atanumajumder2004@gmail.com>
2024-11-11 08:59:39 +0000

    code for custom and single image upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
+'use client'
+
+import { SingleImageDropzone } from "@/components/single-image-dropzone";
+import { useEdgeStore } from "@/lib/edgestore";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [file, setFile] = useState<File>();
+  const [progress, setProgress] = useState(0);
+  const [urls, setUrls] = useState<{
+    url: string;
+    thumbnailUrl: string | null;
+  }>();
+  const { edgestore } = useEdgeStore()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleUpload = async () => {
+    if (file) {
+      const res = await edgestore.myPublicImages.upload({
+        file,
+        onProgressChange: (progress) => {
+          setProgress(progress)
+        }
+      })
+      // save our data here...
+      setUrls({
+        url: res.url,
+        thumbnailUrl: res.thumbnailUrl
+      })
+    }
+  }
+  return (
+    <>
+    <div className="flex flex-col md:flex-row items-center gap-3">
+
+      <div className="flex flex-col items-center m-6 gap-2">
+        <h1 className="text-xl underline font-bold">Custom Made Upload</h1>
+        <input type="file" onChange={(e) => { setFile(e.target.files?.[0]) }} />
+        <div className="h-[6px] w-44 border rounded overflow-hidden">
+          <div className="h-full bg-white transition-all duration-150" style={{ width: `${progress}%` }} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <button className="bg-white text-black rounded px-2 hover:opacity-80" onClick={handleUpload}>Upload</button>
+        {urls?.url && <Link href={urls.url} target="_blank">Image Url</Link>}
+        {urls?.thumbnailUrl && <Link href={urls.thumbnailUrl} target="_blank">Thumbnail Image Url</Link>}
+      </div>
+
+      <div className="flex flex-col items-center m-6 gap-2">
+        <h1 className="text-xl underline font-bold">Single Image Upload</h1>
+        <span>max file size 1Mb</span>
+        <SingleImageDropzone width={200} height={200} value={file} 
+        onChange={(file) => { setFile(file) }} dropzoneOptions={{maxSize: 1024*1024*1}} />
+        <div className="h-[6px] w-44 border rounded overflow-hidden">
+          <div className="h-full bg-white transition-all duration-150" style={{ width: `${progress}%` }} />
+        </div>
+        <button className="bg-white text-black rounded px-2 hover:opacity-80" onClick={handleUpload}>Upload</button>
+        {urls?.url && <Link href={urls.url} target="_blank">Image Url</Link>}
+        {urls?.thumbnailUrl && <Link href={urls.thumbnailUrl} target="_blank">Thumbnail Image Url</Link>}
+      </div>
+
     </div>
+    </>
+
+
   );
 }
